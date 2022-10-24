e76cef6148a67afef472e2424e8a550e873ee536
Esra Kaya <esra.kaya@ovo-wr94g2gcdc.home>
2022-10-24 11:42:58 +0100

    UI-setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
 const App: React.FC = ()=> {
+
   return (
     <div className="App">
-     <span className='heading'>Task Manager</span>
-     
+    <span className='heading'>Task Manager</span>
+    <InputField/>
     </div>
   );
 }
