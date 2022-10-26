fbc7a25384e27b41c3158540c22e1cd0cac8cac9
Esra Kaya <esrakaya165@outlook.com>
2022-10-26 09:33:04 +0100

    array-of-type

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './components/model';
+
 
 const App: React.FC = ()=> {
+  //  we use angled brackets to give ⬇ type 
+  const [todo, setTodo] = useState<string>('')
 
+  // array of a type/interface
+    // Todo can be found in model.ts | ⬇ type is array 
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  
+  console.log(todo)
   return (
     <div className="App">
     <span className='heading'>Task Manager</span>
-    <InputField/>
+    <InputField todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+
+
+// defining Props here to provide the ⬇ types
+const InputField = ({todo, setTodo}:Props) => {
   return (
     <form className='input'>
-      <input type='input' placeholder='Enter a task' className='input_box'/>
+      <input type='input'
+      value={todo}
+      onChange = {(e)=>setTodo(e.target.value)}
+      placeholder='Enter a task' className='input_box'/>
     <button className='input_submit' type='submit'>Add</button>
     </form>
   )
