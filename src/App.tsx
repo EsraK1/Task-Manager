d283336edf077dee62bea90cae525fc69eaca41b
Esra Kaya <esrakaya165@outlook.com>
2022-10-26 11:14:22 +0100

    todo logic

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,23 @@ const App: React.FC = ()=> {
     // Todo can be found in model.ts | ⬇ type is array 
   const [todos, setTodos] = useState<Todo[]>([])
 
-  const handleAdd = () => {}
+  // whereever we send a variable we have to define its type 
+  // for example the type of handleAdd is also defined in inputField.tsx file
+  //               event type ⬇
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo) {
+      //     generating a random id ⬇
+      setTodos([...todos, {id: Date.now(), todo, isDone:false}])
+      // ⬇ once the todo is submitted the todo field is cleared
+      setTodo("");
+    }
+
+  }
 
 
-  console.log(todo)
+  console.log(todos)
   return (
     <div className="App">
     <span className='heading'>Task Manager</span>
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -4,13 +4,15 @@ import "./styles.css";
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>
-  handleAdd:() => void;
+    //      event type ⬇
+  handleAdd:(e: React.FormEvent) => void;
 }
 
 
 // defining Props here to provide the ⬇ types
 const InputField = ({todo, setTodo, handleAdd}:Props) => {
   return (
+    // Whenever this form is submitted the ⬇ handleAdd function is fired
     <form className='input' onSubmit={handleAdd}>
       <input type='input'
       value={todo}
